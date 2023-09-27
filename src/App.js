e5b529a5307cd7ef5a54e2c610c7b83eea081000
Justin <59674603+Toash@users.noreply.github.com>
2023-09-27 13:56:57 -0400

    created react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo List made with React</h1>
+      <p>Hello!</p>
+    </div>
+  );
+}
+
+export default App;
