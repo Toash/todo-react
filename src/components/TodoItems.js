a2265385cb76d51b73d1b55ccafb9cbbe1acf4e3
Justin <59674603+Toash@users.noreply.github.com>
2023-09-27 21:27:38 -0400

    added title for completed list

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -2,17 +2,19 @@
 Displays an array of task items in ordered list
 */
 
-function TodoItems(props){
+function TodoItems(props) {
     // the list of todos.
     const todoItems = props.items;
 
     //goes through every item in the todoItems array and adds them as list item
-    return(
-        <ul>
-            {todoItems.map((item) => <li key={item.key}>
-                {item.text}
-                <button onClick={() => props.delete(item.key)}>Delete</button>
-                </li>)}
+    return (
+        <ul className="d-inline-flex flex-column">
+            {todoItems.map((item) =>
+                <div className="d-flex my-1">
+                    <li key={item.key}>{item.text}</li>
+                    <button onClick={() => props.delete(item.key)}>Delete</button>
+                </div>
+            )}
         </ul>
     )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,18 @@
 import { useState } from "react"
 import TodoItems from "./TodoItems";
 
-function TodoList(){
+function TodoList() {
 
-    const [taskInput,setTaskInput] = useState("")
+    const [taskInput, setTaskInput] = useState("")
     const [taskItems, setTaskItems] = useState([]);
 
-    function addItem(event){
+    function addItem(event) {
         event.preventDefault() //for submit button
 
         //if no input, return
-        if(taskInput.trim() == ""){
+        if (taskInput.trim() == "") {
             return
-        } 
+        }
 
         // object
         const newItem = {
@@ -25,25 +25,35 @@ function TodoList(){
 
 
         setTaskInput('')
-        
+
         event.target.task.focus();
     }
 
-    function deleteItem(key){
+    function deleteItem(key) {
         // Filter the array and excludes all with the key.
         setTaskItems((prevItems) => prevItems.filter((item) => item.key !== key))
     }
 
     return (
         <>
-            <h1>TodoList</h1>
-            <form onSubmit={addItem}>
-                <label htmlFor="task">What to do?</label>
-                <input id="task" type="text" autoFocus value={taskInput} onChange={(e) => setTaskInput(e.target.value)}/>
-                <button type="submit">Add</button>
-            </form>
+            <h1>Todo List</h1>
+            <div className="d-flex justify-content-around">
+                <div>
+                    <form onSubmit={addItem}>
+                        <label className="mx-2 block font-weight-bold" htmlFor="task">What to do?</label>
+                        <div>
+                            <input className="mx-2" id="task" type="text" autoFocus value={taskInput} onChange={(e) => setTaskInput(e.target.value)} />
+                            <button className="mx-2" type="submit">Add</button>
+                        </div>
+                    </form>
+                    <TodoItems items={taskItems} delete={deleteItem} />
+                </div>
+                <div>
+                    <h2>Completed</h2>
+                </div>
+            </div>
 
-            <TodoItems items={taskItems} delete={deleteItem}/>
+
         </>
     )
 }
